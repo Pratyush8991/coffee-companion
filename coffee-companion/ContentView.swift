8c30a5ac82697ee9833d507bf4c7c47889deb88b
Bhattacharya, Pratyush <prbhatt@iu.edu>
2024-09-30 19:53:43 -0400

    Initial Commit

---
+++ coffee-companion/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  coffee-companion
+//
+//  Created by Pratyush Bhattacharya on 9/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ coffee-companion/coffee_companionApp.swift
@@ -0,0 +1,17 @@
+//
+//  coffee_companionApp.swift
+//  coffee-companion
+//
+//  Created by Pratyush Bhattacharya on 9/30/24.
+//
+
+import SwiftUI
+
+@main
+struct coffee_companionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
